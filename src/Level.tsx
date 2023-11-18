a45c39b8a0480fc45e916983df76a3d3e7fb80ce
Frovu <sfrovis@gmail.com>
2023-11-18 15:16:07 +0300

    display level values

--- src/Level.tsx
+++ src/Level.tsx
@@ -1,21 +1,24 @@
-import { useMemo } from 'react';
 import * as THREE from 'three';
 
 function generateLevel(size: number) {
 	const grid = new Uint8Array(size*size);
-	return grid.map(_ => Math.random() * 255);
+	return grid.map(_ => Math.random() * 256);
 }
 export function Level() {
 	
-	const size = 256;
+	const size = 128;
 	const grid = generateLevel(size);
-	const data = new Uint8Array(size * size * 4);
-	for (let i=0; i < grid.length * 4; i += 4) {
-		data[i] = data[i+1] = data[i+2] = grid[i/4];
-		data[i+3] = 255;
+
+	const data = new Uint8ClampedArray(size * size * 2);
+	for (let i = 0; i < grid.length; ++i) {
+		const val = (grid[i] === 255 ? 1 : -Math.log10(-grid[i] / 255 / 4 + 1)) * 256;
+		data[i * 2] = val * 2;
+		data[i * 2 + 1] = val / 3;
 	}
 
-	const dataTexture = new THREE.DataTexture(data, size, size, THREE.RGBAFormat);
+	const dataTexture = new THREE.DataTexture(data, size, size, THREE.RGFormat);
+	dataTexture.colorSpace = THREE.NoColorSpace;
+	dataTexture.flipY = true;
 	dataTexture.needsUpdate = true;
   
 	return <>
